8a08ff0cc591dbcf1fe91385714fef6d84e12a2b
Sandu <bociasan95@gmail.com>
2022-08-16 17:35:11 +0300

    mda

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
     useEffect(()=> {
         console.log(user.username)
-        // dispatch(setUsername('bociasan'))
+        dispatch(setUsername('bociasan'))
         console.log(Object.keys(dispatch))
     }, [])
 
--- src/store.js
+++ src/store.js
@@ -1,14 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit'
 import { createStore } from 'redux'
-import reducer from './reducers'
-import userReducer from './userSlice'
-import inputReducer from './inputSlice'
+import rootReducer from '../reducers'
 
-// export const store = configureStore({
-//     reducer: {
-//         user: userReducer,
-//         input: inputReducer
-//     }
-// })
-
-export const store = createStore(reducer)
+const store = createStore(rootReducer)
