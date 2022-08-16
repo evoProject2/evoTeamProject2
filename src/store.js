27c6571ac93af8933cd67e364507a1c1b4dafc8a
Sandu <bociasan95@gmail.com>
2022-08-16 16:19:31 +0300

    daa

--- src/store.js
+++ src/store.js
@@ -1,14 +1,8 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 
-const reducer = (state, action) {
-    switch(action.type) {
-        case ACTION_1: return { value: action.value_1 };
-        case ACTION_2: return { value: action.value_2 };
 
-        default: return state;
+export const store = configureStore({
+    reducer: {
+        // pizza: pizzaReducer
     }
-}
-
-const store = createStore(reducer);
-
-export default store;
+})
