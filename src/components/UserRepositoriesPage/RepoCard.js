788a36ddeb8f05fd6aec7b735825816e8598457e
Dănilă Ana-Maria <anamaria1616d@gmail.com>
2022-08-16 18:18:29 +0300

    card background changed

--- src/components/UserRepositoriesPage/RepoCard.js
+++ src/components/UserRepositoriesPage/RepoCard.js
@@ -1,20 +1,47 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import Paper from "@mui/material/Paper";
+import { useTheme } from "@mui/material/styles";
 
-export default function OutlinedCard() {
+export default function RepoCard() {
+  const theme = useTheme();
   return (
-    <Box sx={{ minWidth: 275 }}>
+    <Paper
+      sx={{
+        width: "100%",
+        minHeight: "100vh",
+        backgroundColor: theme.palette.background,
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "column",
+        borderRadius: "0px",
+        overflowX: "hidden",
+      }}
+      elevation={2}
+    >
       <Card
         sx={{
-          height: "300px",
+          minWidth: 275,
+          height: "30em",
+          backgroundColor: "black",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Box sx={{ backgroundColor: "pink" }}>aa</Box>
+        <Box
+          sx={{
+            minWidth: 150,
+            backgroundColor: "pink",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          aa
+        </Box>
       </Card>
-    </Box>
+    </Paper>
   );
 }
