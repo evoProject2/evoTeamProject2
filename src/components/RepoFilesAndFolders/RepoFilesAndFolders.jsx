469a37ec29f9564a3adf6ef0bd57f8306fa47cd1
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-18 12:21:16 +0300

    Fix conflictes

--- src/components/RepoFilesAndFolders/RepoFilesAndFolders.jsx
+++ src/components/RepoFilesAndFolders/RepoFilesAndFolders.jsx
@@ -2,26 +2,31 @@ import React, { useState, useEffect } from "react";
 
 const RepoFilesAndFolders = ({ repo }) => {
   const [filesAndFolders, setFilesAndFolder] = useState([]);
-
-  const fileApi = `https://api.github.com/repos/${repo.full_name}/contents`;
+  console.log(repo);
+  const repoContentApi = `https://api.github.com/repos/${repo.full_name}/contents`;
+  // const fileApi = `https://api.github.com/repos/${repo.full_name}/contents/${repo.git_url}`;
 
   const getRepoContent = async () => {
-    const data = await fetch(fileApi);
+    const data = await fetch(repoContentApi);
     const res = await data.json();
+    console.log(res);
     if (!res.documentation_url) {
       // Fix the object problem when I have an empty repo
       setFilesAndFolder(res);
     }
   };
+
   useEffect(() => {
     getRepoContent();
   }, []);
 
   return (
     <div>
-      <ul style={{ listStyle: "none" }}>
+      <ul style={{ listStyle: "none", padding: "0" }}>
         {filesAndFolders.map((el) => (
-          <li key={el.name}>{el.name}</li>
+          <li key={el.name} style={{ padding: "0.3rem 0" }}>
+            {el.name}
+          </li>
         ))}
       </ul>
     </div>
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -10,7 +10,7 @@ const UserDetails = () => {
   const userAbout = user.userAbout;
 
   useEffect(() => {
-    console.log(userAbout);
+    // console.log(userAbout);
   }, []);
 
   return (
@@ -21,6 +21,7 @@ const UserDetails = () => {
           sx={{
             height: "300px",
             width: "300px",
+            outline: "3px solid #fff",
           }}
         />
 
