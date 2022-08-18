d600596994b47e9525c911417bf84e959d14e714
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-08-18 12:28:26 +0300

    Fixed issue

--- src/reducers/filterSlice.js
+++ src/reducers/filterSlice.js
@@ -3,7 +3,6 @@ import { FILTER } from "../components/FilterBar/filterConstants";
 import { filterRepos } from "../components/FilterBar/filterFunctions";
 
 const initialState = {
-<<<<<<< HEAD
   inputValue: "",
   show: false,
   filterToggle: false,
@@ -53,47 +52,3 @@ export const {
   setNeedFilterFlag,
 } = filterSlice.actions;
 export default filterSlice.reducer;
-=======
-    inputValue: '',
-    show: false,
-    needFilterFlag: false,
-    filters: [],
-    sorting: {
-        type: 'none',
-        direction: 'ascending'
-    },
-}
-
-export const filterSlice = createSlice({
-    name: 'input',
-    initialState,
-    reducers: {
-        setInputValue: (state, newValue) => {
-            state.inputValue = newValue.payload
-        },
-        showFiltersToggle: (state) => {
-            state.show = !state.show
-        },
-        setSortingDirection: (state, direction) => {
-            state.sorting.direction = direction.payload
-        },
-        setSortingType: (state, type) => {
-            state.sorting.type = type.payload
-        },
-        setNeedFilterFlag: (state, flag) => {
-            state.needFilterFlag = flag.payload
-        }
-
-
-    }
-})
-
-export const {
-    setInputValue,
-    setSortingType,
-    setSortingDirection,
-    showFiltersToggle,
-    setNeedFilterFlag,
-} = filterSlice.actions
-export default filterSlice.reducer
->>>>>>> 5ef939394058e8936a44ecebe27dc4762061bd4d
