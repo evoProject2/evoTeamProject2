d9bfd5e2374471a3ff917c3faee7e5ee1b265541
Sandu <bociasan95@gmail.com>
2022-08-23 15:56:23 +0300

    Deleted switch from filter

--- src/pages/UserRepositories/UserRepositories.jsx
+++ src/pages/UserRepositories/UserRepositories.jsx
@@ -1,93 +1,96 @@
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import RepoCard from "../../components/RepoCard/RepoCard";
-import { useEffect } from "react";
-import { getGithubLanguageColors } from "../../utils/functions";
-import { setGithubColors } from "../../utils/reducers/githubSlice";
+import {useEffect} from "react";
+import {getGithubLanguageColors} from "../../utils/functions";
+import {setGithubColors} from "../../utils/reducers/githubSlice";
 import FilterLogic from "../../components/FilterLogic/FilterLogic";
 import Box from "@mui/material/Box";
 import UserDetails from "../../components/RepoCard/UserDetails";
 import SearchRepoBar from "../../components/SearchRepoBar/SearchRepoBar";
-import { useNavigate } from "react-router";
+import {useNavigate} from "react-router";
+import {showFiltersToggle} from "../../utils/reducers/filterSlice";
 
 const UserRepositories = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const userAbout = user.userAbout;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
+    const filter = useSelector((state) => state.filter);
+    const userAbout = user.userAbout;
 
-  useEffect(() => {
-    if (Object.keys(userAbout).length === 0) {
-      navigate("/", { replace: true });
-    } else {
-      getGithubLanguageColors().then((colors) =>
-        dispatch(setGithubColors(colors))
-      );
-    }
-  }, []);
+    useEffect(() => {
+        if (Object.keys(userAbout).length === 0) {
+            navigate("/", {replace: true});
+        } else {
+            dispatch(showFiltersToggle())
+            getGithubLanguageColors().then((colors) =>
+                dispatch(setGithubColors(colors))
+            );
+        }
+    }, []);
 
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        <Box sx={{ width: "100%" }}>
-          <UserDetails />
-        </Box>
-
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            "@media(max-width: 960px)": {
-              width: "75%",
-            },
-          }}
-        >
-          <SearchRepoBar />
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            margin: "20px",
-            justifyContent: "flex-start",
-            width: "90%",
-            "@media(min-width: 500px)": {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            },
-          }}
-        >
-          {user.filteredRepositories &&
-            user.filteredRepositories.map((repo, index) => (
-              <Box
+    return (
+        <>
+            <Box
                 sx={{
-                  "@media(max-width: 500px)": {
                     display: "flex",
-                    justifyContent: "center",
+                    flexDirection: "column",
                     alignItems: "center",
                     width: "100%",
-                  },
                 }}
-                key={`${repo.name}-${index}`}
-              >
-                <RepoCard repo={repo} sx={{}} />
-              </Box>
-            ))}
-        </Box>
-      </Box>
-      <FilterLogic />
-    </>
-  );
+            >
+                <Box sx={{width: "100%"}}>
+                    <UserDetails/>
+                </Box>
+
+                <Box
+                    sx={{
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        "@media(max-width: 960px)": {
+                            width: "75%",
+                        },
+                    }}
+                >
+                    <SearchRepoBar/>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        margin: "20px",
+                        justifyContent: "flex-start",
+                        width: "90%",
+                        "@media(min-width: 500px)": {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        },
+                    }}
+                >
+                    {user.filteredRepositories &&
+                    user.filteredRepositories.map((repo, index) => (
+                        <Box
+                            sx={{
+                                "@media(max-width: 500px)": {
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    width: "100%",
+                                },
+                            }}
+                            key={`${repo.name}-${index}`}
+                        >
+                            <RepoCard repo={repo} sx={{}}/>
+                        </Box>
+                    ))}
+                </Box>
+            </Box>
+            <FilterLogic/>
+        </>
+    );
 };
 
 export default UserRepositories;
