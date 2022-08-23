5c70759e7a7b7a5cd48593045a0d88de56593c5a
Dănilă Ana-Maria <anamaria1616d@gmail.com>
2022-08-24 01:18:56 +0300

    Implemented search pagination

--- src/components/LanguagesBar/LanguagesBar.jsx
+++ src/components/LanguagesBar/LanguagesBar.jsx
@@ -1,74 +1,73 @@
 import "./LanguageBar.css";
-import {Box, createStyles, Tooltip} from "@mui/material";
+import { Box, createStyles, Tooltip } from "@mui/material";
 
-export const LanguagesBar = ({languages, sum, colors, repoName}) => {
-    return (
-        Object.keys(languages).length > 0 && (
-            <Box
-                key={repoName + "lang"}
-                className={"language-bar-component-container"}
+export const LanguagesBar = ({ languages, sum, colors, repoName }) => {
+  return (
+    Object.keys(languages).length > 0 && (
+      <Box
+        key={repoName + "lang"}
+        className={"language-bar-component-container"}
+      >
+        <Box className={"language-bar-title"}>Languages</Box>
+        <Box className={"language-bar-container"}>
+          {Object.keys(languages).map((language) => (
+            <Tooltip
+              key={repoName + "%" + language}
+              PopperProps={{
+                sx: {
+                  // marginBottom: "-5px !important",
+                },
+              }}
+              arrow
+              placement="top"
+              title={
+                <Box
+                  // key={repoName + "%" + language}
+                  className={"language-bar-percentages-row"}
+                >
+                  <Box
+                    className={"language-circle"}
+                    style={{ backgroundColor: colors[language]?.color }}
+                  />
+                  <Box className={"language-name"}> {language} </Box>
+                  <Box className={"language-percentage"}>
+                    {`${((languages[language] / sum) * 100).toFixed(1)}%`}
+                  </Box>
+                </Box>
+              }
             >
-                <Box className={"language-bar-title"}>Languages</Box>
-                <Box className={"language-bar-container"}>
-                    {Object.keys(languages).map((language) => (
-                        <Tooltip
-                            key={repoName + "%" + language}
-                            PopperProps={{
-                                sx: {
-                                    // marginBottom: "-5px !important",
-                                },
-                            }}
-                            arrow
-                            placement="top"
-                            title={
-                                <Box
-                                    // key={repoName + "%" + language}
-                                    className={"language-bar-percentages-row"}
-                                >
-                                    <Box
-                                        className={"language-circle"}
-                                        style={{backgroundColor: colors[language].color}}
-                                    />
-                                    <Box className={"language-name"}> {language} </Box>
-                                    <Box className={"language-percentage"}>
-                                        {`${((languages[language] / sum) * 100).toFixed(1)}%`}
-                                    </Box>
-                                </Box>
-                            }
-                        >
+              <Box
+                key={repoName + "bar" + language}
+                className={"language-segment"}
+                style={{
+                  width: `${(languages[language] / sum) * 100 + 3}%`,
+                  backgroundColor: colors[language]?.color,
+                }}
+              />
+            </Tooltip>
+          ))}
+        </Box>
 
-                            <Box
-                                key={repoName + "bar" + language}
-                                className={"language-segment"}
-                                style={{
-                                    width: `${(languages[language] / sum) * 100 + 3}%`,
-                                    backgroundColor: colors[language].color,
-                                }}
-                            />
-                        </Tooltip>
-                    ))}
-                </Box>
-
-                <Box className={"language-bar-percentages-container"}>
-                    {Object.keys(languages).map((language) => (
-                        <Box
-                            key={repoName + "%" + language}
-                            className={"language-bar-percentages-row"}
-                        >
-                            <Box
-                                className={"language-circle"}
-                                style={{backgroundColor: colors[language].color}}
-                            />
-                            <Box className={"language-name"}> {language} </Box>
-                            <Box className={"language-percentage"}>
-                                {`${((languages[language] / sum) * 100).toFixed(1)}%`}
-                            </Box>
-                        </Box>
-                    ))}
-                </Box>
+        <Box className={"language-bar-percentages-container"}>
+          {Object.keys(languages).map((language) => (
+            <Box
+              key={repoName + "%" + language}
+              className={"language-bar-percentages-row"}
+            >
+              <Box
+                className={"language-circle"}
+                style={{ backgroundColor: colors[language]?.color }}
+              />
+              <Box className={"language-name"}> {language} </Box>
+              <Box className={"language-percentage"}>
+                {`${((languages[language] / sum) * 100).toFixed(1)}%`}
+              </Box>
             </Box>
-        )
-    );
+          ))}
+        </Box>
+      </Box>
+    )
+  );
 };
 
 export default LanguagesBar;
--- src/components/UsersDisplay.js
+++ src/components/UsersDisplay.js
@@ -1,19 +1,55 @@
+import { useState } from "react";
+
 import UserCard from "./UserCard";
 import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Box from "@mui/material/Box";
+
 import LoadinguUsers from "./LoadingUsers";
+import { USERS_PER_PAGE } from "../utils/constants";
 
 const UsersDisplay = ({ users, loading }) => {
+  const [page, setPage] = useState(1);
+
   const noResults = users.length === 0;
-  return (
-    <>
-      {loading ? (
-        <LoadinguUsers />
-      ) : !noResults ? (
-        users.map((user) => <UserCard key={user.id} userInfo={user} />)
-      ) : (
-        <Typography variant="h5">No results found</Typography>
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const totalPages = Math.ceil(users.length / USERS_PER_PAGE);
+  console.log(totalPages);
+
+  const usersToDisplay = users.slice(
+    (page - 1) * USERS_PER_PAGE,
+    page * USERS_PER_PAGE
+  );
+
+  return loading ? (
+    <LoadinguUsers />
+  ) : !noResults ? (
+    <Box
+      sx={{
+        marginTop: "30px",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {usersToDisplay.map((user) => (
+        <UserCard key={user.id} userInfo={user} />
+      ))}
+      {totalPages > 1 && (
+        <Pagination
+          count={totalPages}
+          page={page}
+          onChange={handlePageChange}
+        />
       )}
-    </>
+    </Box>
+  ) : (
+    <Typography variant="h5">No results found</Typography>
   );
 };
 
